1f6bc21a275a0e52bb39d43e78667bb8815c4c23
Anu-tochka <anuta-zh@mail.ru>
2023-08-31 23:43:31 +0300

    Add file

---
+++ server.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string.h>
+#include <mysql.h>
+ 
+using namespace std;
+ 
+void startConnect()  { 
+	MYSQL mysql;
+	MYSQL_RES* res;
+	MYSQL_ROW row;
+ 
+	// Получаем дескриптор соединения
+	mysql_init(&mysql);
+	if (&mysql == nullptr) {
+		// Если дескриптор не получен — выводим сообщение об ошибке
+		cout << "Error: can't create MySQL-descriptor" << endl;
+	}
+ 
+	// Подключаемся к серверу
+	if (!mysql_real_connect(&mysql, "localhost", "root", "root", "chat", NULL, NULL, 0)) {
+		// Если нет возможности установить соединение с БД выводим сообщение об ошибке
+		cout << "Error: can't connect to database " << mysql_error(&mysql) << endl;
+	}
+} 
+ 
+void autorisation(string login, string pass)  { 
+	mysql_query(&mysql, "SELECT id, name FROM users WHERE login=" + login + " AND pass=" + pass); //Делаем запрос к таблице
+	if (res = mysql_store_result(&mysql)) {
+		string name = mysql_fetch_row(res); 
+		cout << "Hello, " << name << "!";
+	}
+	else
+		cout << "Неправильный логин или пароль";
+} 
+ 
+void messages(string batch)  {	
+	mysql_query(&mysql, "SELECT * FROM table_fromCplusplus"); //Делаем запрос к таблице
+ 
+	//Выводим все что есть в таблице через цикл
+	if (res = mysql_store_result(&mysql)) {
+		while (row = mysql_fetch_row(res)) {
+			for (i = 0; i < mysql_num_fields(res); i++) {
+				cout << row[i] << "  ";
+			}
+			cout << endl;
+		}
+	}
+	else
+		cout << "Ошибка MySql номер " << mysql_error(&mysql);
+} 
+ 
+void closeConnect()  {	 
+	// Закрываем соединение с базой данных
+	mysql_close(&mysql);
+ 
+}
